899dd3ab7be073231470ef063f20867b03a3863e
mattiesvzw <matthiasseghers@icloud.com>
2016-03-20 19:33:42 +0100

    Stukjes van Tests

--- IIM/Global.asax.cs
+++ IIM/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IIM.Models.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,9 @@ namespace IIM
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            IIMContext db = new IIMContext();
+            //db.Database.Initialize(true);
         }
     }
 }
