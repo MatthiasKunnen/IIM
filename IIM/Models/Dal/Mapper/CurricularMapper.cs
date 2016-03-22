a18d83b8a8ebe5f1f07834633792f684f811624e
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-03-22 04:56:12 +0100

    Removed unused imports

--- IIM/Models/Dal/Mapper/CurricularMapper.cs
+++ IIM/Models/Dal/Mapper/CurricularMapper.cs
@@ -1,9 +1,5 @@
 ﻿using IIM.Models.Domain;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Web;
 
 namespace IIM.Models.DAL.Mapper
 {
