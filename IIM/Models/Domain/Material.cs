2d32ec54512040ee30567afa180f8c176841b85b
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-03-22 02:02:04 +0100

    Fixed photo of Material

--- IIM/Models/Domain/Material.cs
+++ IIM/Models/Domain/Material.cs
@@ -1,29 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace IIM.Models.Domain
 {
     public class Material
     {
-        
-        public string Name { get; private set; }
-
-        public string Description { get; private set; }
-
-        public Firm Firm { get; private set; }
-
-        public decimal Price { get; set; }
-
         public string ArticleNr { get; private set; }
-
-        public int Id { get; private set; }
-
-        public string Encoding { get; private set; }
-
         public List<Curricular> Curriculars { get; private set; }
-
-        public List<TargetGroup> TargetGroups { get; private set; }
-
+        public string Description { get; private set; }
+        public string Encoding { get; private set; }
+        public Firm Firm { get; private set; }
+        public int Id { get; private set; }
         public int Identifiers { get; private set; }
+        public string Name { get; private set; }
+        public string PhotoUrl => this.Id == 0 || this.Encoding == null ? null : $"https://iim.blob.core.windows.net/images/{this.Id}.{this.Encoding}";
+        public decimal Price { get; private set; }
+        public List<TargetGroup> TargetGroups { get; private set; }
     }
 }
--- IIM/ViewModels/MaterialViewModel.cs
+++ IIM/ViewModels/MaterialViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using IIM.Models.Domain;
 using System.ComponentModel.DataAnnotations;
 
@@ -9,26 +8,29 @@ namespace IIM.ViewModels
 {
     public class MaterialViewModel
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int Amount { get; set; }
         public string ArticleNr { get; set; }
+        public List<Curricular> Curriculars { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public Firm Firm { get; set; }
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+
         [DisplayFormat(DataFormatString = "{0:c}")]
         public decimal Price { get; set; }
-        public Firm Firm { get; set; }
         public List<TargetGroup> TargetGroups { get; set; }
-        public List<Curricular> Curriculars { get; set; }
-        public MaterialViewModel() { }
 
         public MaterialViewModel(Material m)
         {
-            this.Name = m.Name;
-            this.Description = m.Description;
-            this.ArticleNr = m.ArticleNr;
-            this.Price = m.Price;
-            this.Firm = m.Firm;
-            this.TargetGroups = m.TargetGroups;
-            this.Curriculars = m.Curriculars;
+            ArticleNr = m.ArticleNr;
+            Curriculars = m.Curriculars;
+            Description = m.Description;
+            Image = m.Encoding;
+            Firm = m.Firm;
+            Name = m.Name;
+            PhotoUrl = m.PhotoUrl;
+            Price = m.Price;
+            TargetGroups = m.TargetGroups;
         }
     }
 }
