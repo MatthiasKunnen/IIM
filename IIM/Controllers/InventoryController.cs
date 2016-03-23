d6fab6e7fafdd5a92b349f6f9cecdf36e8841624
PWillaert <pieterwillaert@hotmail.com>
2016-03-23 23:16:53 +0100

    Zoeken op naam in materiaal

--- IIM/Controllers/InventoryController.cs
+++ IIM/Controllers/InventoryController.cs
@@ -1,4 +1,5 @@
-﻿using IIM.Models.Domain;
+﻿using System;
+using IIM.Models.Domain;
 using IIM.ViewModels;
 using System.Linq;
 using System.Web.Mvc;
@@ -14,8 +15,41 @@ namespace IIM.Controllers
         }
 
         // GET
-        public ActionResult Index()
+        public ActionResult Index(string searchName, string searchCurricular)
         {
+            if (!String.IsNullOrEmpty(searchName) && String.IsNullOrEmpty(searchCurricular))
+            {
+                return View(_materialRepository
+                .FindAll()
+                .Where( m => m.Name.Contains(searchName) ||
+                             m.Description.Contains(searchName))
+                .OrderBy(m => m.Name)
+                .ToList()
+                .Select(m => new MaterialViewModel(m))
+                .ToList());
+            }
+            if (String.IsNullOrEmpty(searchName) && !String.IsNullOrEmpty(searchCurricular))
+            {
+                return View(_materialRepository
+                .FindAll()
+                
+                .OrderBy(m => m.Name)
+                .ToList()
+                .Select(m => new MaterialViewModel(m))
+                .ToList());
+            }
+            if (!String.IsNullOrEmpty(searchName) && !String.IsNullOrEmpty(searchCurricular))
+            {
+                return View(_materialRepository
+                .FindAll()
+                .Where(m => m.Name.Contains(searchName) ||
+                            m.Description.Contains(searchName))
+                .OrderBy(m => m.Name)
+                .ToList()
+                .Select(m => new MaterialViewModel(m))
+                .ToList());
+            }
+
             return View(_materialRepository
                 .FindAll()
                 .OrderBy(m => m.Name)
