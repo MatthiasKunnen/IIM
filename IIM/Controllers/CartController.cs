133d7e26b8f30e830bcdbb4e59fae164b7f9bae7
Evert De Vos <evertdevosopwijk@gmail.com>
2016-04-11 18:37:52 +0200

    added cartcontroller

---
+++ IIM/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using IIM.Models.DAL;
+using IIM.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IIM.Controllers
+{
+    public class CartController : Controller
+    {
+        IUserRepository _userRepository;
+
+        public CartController(IUserRepository users)
+        {
+            _userRepository = users;
+        }
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
