fbd9bbf6e6f3afe00804e55f166a0dc167c96eef
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-04-17 04:56:15 +0200

    Completed task 14. ImageStorageUrl in web.config

--- IIM/ViewModels/MaterialViewModel.cs
+++ IIM/ViewModels/MaterialViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using IIM.Models.Domain;
 using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 using System.Web.Mvc;
 
 namespace IIM.ViewModels
@@ -13,7 +14,7 @@ namespace IIM.ViewModels
         [Display(Name = "Artikelnummer")]
 
         public string ArticleNr { get; set; }
-        [Display(Name= "Opleidingsonderdelen")]
+        [Display(Name = "Opleidingsonderdelen")]
         public List<Curricular> Curriculars { get; set; }
         [Display(Name = "Beschrijving")]
         public string Description { get; set; }
@@ -26,8 +27,8 @@ namespace IIM.ViewModels
         public string Name { get; set; }
         public string PhotoUrl
         {
-            get { return _photoUrl; }
-            set { _photoUrl = value ?? "~/Content/photo-coming-soon.jpg"; }
+            get { return _photoUrl ?? "~/Content/photo-coming-soon.jpg"; }
+            set { _photoUrl = value; }
         }
 
         [DisplayFormat(DataFormatString = "{0:c}")]
@@ -46,7 +47,7 @@ namespace IIM.ViewModels
             Image = m.Encoding;
             Firm = m.Firm;
             Name = m.Name;
-            PhotoUrl = m.PhotoUrl;
+            PhotoUrl = $"{ConfigurationManager.AppSettings["ImageStorageUrl"]}/{m.Id}.{m.Encoding}";
             Price = m.Price;
             TargetGroups = m.TargetGroups;
         }
